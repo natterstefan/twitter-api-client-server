5f231647a2f562d46b6d55e112da01c2ba907c88
Stefan Natter <stefan@natter.at>
2021-02-13 13:34:24 +0100

    test: first draft of the twitterClient mock

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -2,6 +2,8 @@ import request from 'supertest'
 
 import app from '../server'
 
+jest.mock('../utils/twitterClient')
+
 describe('GET /', () => {
   // eslint-disable-next-line jest/no-done-callback
   it('should return 200 OK', done => {
---
+++ src/utils/__mocks__/twitterClient.ts
@@ -0,0 +1,4 @@
+/**
+ * TODO: extend mocks
+ */
+export const twitterClient = {}
